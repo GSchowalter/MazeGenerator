dd2a8f503c5d8371e5222c9450af6db1642004a4
Grant Schowalter <gschowalter@Grants-MacBook-Pro.local>
2020-01-26 20:36:37 -0500

    Writing maze class

---
+++ MazeGen.py
@@ -0,0 +1,15 @@
+
+class Maze:
+
+   #Initialize a 2d array to hold a prim's algorithm generated maze
+   #1 indicates path
+   #0 wall
+   def __init__(self, width, height, maze):
+       self.maze[][]
+       for i in range width:
+           for j in range height:
+               self.maze[i][j] = 0
+
+
+
+        
