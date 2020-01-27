23777d728a92590879a64157f73cf065c61e8ce7
Grant Schowalter <gschowalter@Grants-MacBook-Pro.local>
2020-01-26 20:57:48 -0500

    Added Cell class

---
+++ Cell.py
@@ -0,0 +1,18 @@
+class Cell:
+
+    def __init__(x, y):
+        self.x = x
+        self.y = y
+        wall = True
+
+    def getWall():
+        return self.wall
+
+    def getX():
+        return self.x
+    
+    def getY():
+        return self.y
+    
+    def getPos():
+        return [getX(), getY()]
--- MazeGen.py
+++ MazeGen.py
@@ -1,19 +1,25 @@
 
 class Maze:
 
+
    #Initialize a 2d array to hold a prim's algorithm generated maze
    #1 indicates path
    #0 wall
    #Initialized to all walls and then the algorithm modifies 
-   def __init__(self, width, height, maze):
-       self.maze[][]
-       for i in range width:
-           for j in range height:
+    def __init__(self, width, height, maze):
+        for i in range(width):
+           for j in range(height):
                self.maze[i][j] = 0
-        prim()
+
+        __prim__()
 
     #Modifies the grid to be a maze
-    def __prim__()
+    def __prim__():
+        walls = [[]]
+
+    def add_walls(cell):
+        
+
 
 
 
