b8d08b03d40c91623df4596f991d296cc5a7cfd6
Grant Schowalter <gschowalter@Grants-MacBook-Pro.local>
2020-01-26 20:13:22 -0500

    Updated screen color and added update

--- MazeGen.py
+++ MazeGen.py
@@ -6,9 +6,13 @@ pygame.init()
 #Initialize constants
 game_width = 800
 game_height = 400
+screen_color = (8, 7, 97)
 
 #Create screen
 screen = pygame.display.set_mode((game_width,game_height))
+
+#Title and icon
+pygame.display.set_caption("Maze")
 
 #Game Loop
 running = True
@@ -16,3 +20,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #Set display background
+    screen.fill(screen_color)
+    pygame.display.update()
