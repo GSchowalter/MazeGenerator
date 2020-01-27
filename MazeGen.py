efe56fa47d488209e8d8167fc4ff3f4cee9db178
Grant Schowalter <gschowalter@Grants-MacBook-Pro.local>
2020-01-26 20:03:33 -0500

    Establish screen and game loop

--- MazeGen.py
+++ MazeGen.py
@@ -1,1 +1,18 @@
 import pygame
+
+#Initialize pygame
+pygame.init()
+
+#Initialize constants
+game_width = 800
+game_height = 400
+
+#Create screen
+screen = pygame.display.set_mode((game_width,game_height))
+
+#Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
