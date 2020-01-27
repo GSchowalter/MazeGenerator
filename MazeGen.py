712604ad933493903df2faa0baade3200ed3d87f
Grant Schowalter <gschowalter@Grants-MacBook-Pro.local>
2020-01-26 20:48:26 -0500

    building prims

--- MazeGen.py
+++ MazeGen.py
@@ -4,11 +4,16 @@ class Maze:
    #Initialize a 2d array to hold a prim's algorithm generated maze
    #1 indicates path
    #0 wall
+   #Initialized to all walls and then the algorithm modifies 
    def __init__(self, width, height, maze):
        self.maze[][]
        for i in range width:
            for j in range height:
                self.maze[i][j] = 0
+        prim()
+
+    #Modifies the grid to be a maze
+    def __prim__()
 
 
 
