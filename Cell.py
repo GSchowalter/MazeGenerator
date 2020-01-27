b990265dd64809a63d46d6773f1f61ec2e3b67d7
Grant Schowalter <gschowalter@Grants-MacBook-Pro.local>
2020-01-26 21:21:40 -0500

    made getNumberOfNeighbors

--- Cell.py
+++ Cell.py
@@ -1,8 +1,12 @@
 class Cell:
 
-    def __init__(x, y):
+    #Store the width and height of the plane so that I can return the neighbors from each cell
+    #X and Y are position in the plane
+    def __init__(x, y, width, height):
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
         wall = True
 
     def getWall():
@@ -16,3 +20,13 @@ class Cell:
     
     def getPos():
         return [getX(), getY()]
+
+    def getNumberOfNeighbors():
+        #Corners
+        if (((x == width - 1) and (y == height - 1)) or ((x == 0) and (y == height - 1)) or ((x == width - 1) and (y == 0)) or ((x == 0) and (y == 0))):
+            return 3
+        #Sides
+        elif ((x == 0) or (x == width - 1) or (y = 0) or (y == height - 1)):
+            return 5
+        else:
+            return 8
--- MazeGen.py
+++ MazeGen.py
@@ -3,21 +3,23 @@ class Maze:
 
 
    #Initialize a 2d array to hold a prim's algorithm generated maze
-   #1 indicates path
-   #0 wall
    #Initialized to all walls and then the algorithm modifies 
     def __init__(self, width, height, maze):
         for i in range(width):
            for j in range(height):
-               self.maze[i][j] = 0
+               self.maze[i][j] = Cell(width, height)
 
+        walls = []
         __prim__()
 
     #Modifies the grid to be a maze
     def __prim__():
-        walls = [[]]
+        
 
+    #Given a cell this function marks the surrounding cell's as walls by adding them to the wall list
     def add_walls(cell):
+        if ()
+
         
 
 
